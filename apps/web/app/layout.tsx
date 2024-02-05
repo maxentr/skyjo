37574725cf40001e06a70b9b1abd7a6335a4e1ee
maxentr <rouaultmaxent@gmail.com>
2024-02-05 22:02:52 +0100

    chore(web): add a head for SEO

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -12,7 +12,31 @@ const inter = Inter({
 export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
   return (
     <html lang="en" suppressHydrationWarning className={inter.className}>
-      <head />
+      <head>
+        <title>Skyjo - Play Online with Friends</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="Play Skyjo online with friends for free!"
+        />
+        <meta
+          name="keywords"
+          content="Skyjo, online, game, multiplayer, free, play with friends"
+        />
+        <meta name="author" content="Maxent" />
+
+        <meta property="og:title" content="Skyjo - Play Online with Friends" />
+        <meta
+          property="og:description"
+          content="Play Skyjo online with friends for free!"
+        />
+        <meta property="og:url" content="https://skyjo-game.vercel.app/" />
+        <meta property="og:type" content="website" />
+
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+      </head>
       <body className="h-dvh bg-background antialiased">
         <Providers>{children}</Providers>
       </body>
