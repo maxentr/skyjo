6fb05ebb27e23ce54e3d0ca3b10402d63d3104da
maxentr <rouaultmaxent@gmail.com>
2024-02-09 22:48:54 +0100

    style(web): allows user to close score summary between rounds

--- apps/web/components/EndRoundDialog.tsx
+++ apps/web/components/EndRoundDialog.tsx
@@ -2,31 +2,34 @@ import ScoreTable from "@/components/ScoreTable"
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog"
 import { useSkyjo } from "@/contexts/SkyjoContext"
+import { useEffect, useState } from "react"
 
 type EndRoundDialogProps = {}
 
 const EndRoundDialog = ({}: EndRoundDialogProps) => {
   const { game } = useSkyjo()
+  const [open, setOpen] = useState(false)
 
   const isRoundOver = game.roundState === "over" && game.status !== "finished"
 
+  useEffect(() => {
+    setOpen(isRoundOver)
+  }, [isRoundOver])
+
   return (
-    <Dialog open={isRoundOver}>
-      <DialogContent allowClose={false}>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent>
         <DialogHeader>
           <DialogTitle className="text-center">Fin de la manche</DialogTitle>
-          <DialogDescription>
-            <p className="mt-2">
-              La manche suivante va commencer dans quelques secondes.
-            </p>
-            <ScoreTable players={game.players} />
-          </DialogDescription>
         </DialogHeader>
+        <div className="mt-2">
+          La manche suivante va commencer dans quelques secondes.
+        </div>
+        <ScoreTable players={game.players} />
       </DialogContent>
     </Dialog>
   )
