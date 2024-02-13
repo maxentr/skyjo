b8b7ec706bcb1722ffc7aa56f6d72414d67404ea
maxentr <rouaultmaxent@gmail.com>
2024-02-13 12:44:45 +0100

    fix(api): update INITIAL_TURNED_COUNT constant

--- apps/api/src/constants.ts
+++ apps/api/src/constants.ts
@@ -2,7 +2,7 @@ export const CardConstants = {
   PER_ROW: 3,
   PER_COLUMN: 4,
   PER_PLAYER: 3 * 4,
-  INITIAL_TURNED_COUNT: 10,
+  INITIAL_TURNED_COUNT: 2,
 } as const
 
 export const MIN_PLAYERS = 2
