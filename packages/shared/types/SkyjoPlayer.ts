f4815580f094c7735e764344d73581a6d64df306
Maxent <67283154+Maxentr@users.noreply.github.com>
2024-02-18 03:26:08 +0100

    chore(shared): delete types/SkyjoPlayer.ts

--- packages/shared/types/SkyjoPlayer.ts
+++
@@ -1,8 +0,0 @@
-import { PlayerToJson } from "./player"
-import { SkyjoCardToJson } from "./skyjoCard"
-
-export interface SkyjoPlayerToJson extends PlayerToJson {
-  readonly scores: number[]
-  readonly currentScore: number
-  readonly cards: SkyjoCardToJson[][]
-}
