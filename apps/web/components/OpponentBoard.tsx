657611d64f8d1fc2a0a7b6db9655d796b2c245db
maxentr <rouaultmaxent@gmail.com>
2024-03-05 16:39:10 +0100

    feat: add icon and explanation tooltip when opponent has lost connection

--- apps/web/components/OpponentBoard.tsx
+++ apps/web/components/OpponentBoard.tsx
@@ -1,5 +1,12 @@
 import { CardTable } from "@/components/CardTable"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 import { cn } from "@/lib/utils"
+import { AlertTriangleIcon } from "lucide-react"
 import Image from "next/image"
 import { SkyjoPlayerToJson } from "shared/types/skyjoPlayer"
 
@@ -25,11 +32,23 @@ const OpponentBoard = ({ opponent, isPlayerTurn }: OpponentBoardProps) => {
       />
       <p
         className={cn(
-          "text-center select-none text-sm mb-4",
+          "text-center select-none text-sm mb-4 flex flex-row items-center gap-1",
           isPlayerTurn && "font-semibold",
         )}
       >
         {opponent.name}
+        {opponent.connectionStatus === "connection-lost" && (
+          <TooltipProvider delayDuration={200}>
+            <Tooltip>
+              <TooltipTrigger className="relative">
+                <AlertTriangleIcon size={16} className="text-yellow-700" />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Connexion instable</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        )}
       </p>
       <CardTable cards={opponent.cards} size="small" cardDisabled={true} />
     </div>
