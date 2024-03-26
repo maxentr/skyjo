22ad7c5469ce752489443681238337108452315a
maxentr <rouaultmaxent@gmail.com>
2024-03-26 16:36:51 +0100

    refactor(web): reverse chat message list to always see last messages

---
+++ apps/web/components/ChatMessageList.tsx
@@ -0,0 +1,39 @@
+import ChatMessage from "@/components/ChatMessage"
+import { useSkyjo } from "@/contexts/SkyjoContext"
+import { useTranslations } from "next-intl"
+
+type ChatMessageListProps = Readonly<{
+  unreadMessages: ChatMessage[]
+}>
+
+function ChatMessageList({ unreadMessages }: ChatMessageListProps) {
+  const { chat } = useSkyjo()
+  const t = useTranslations("components.ChatMessageList")
+
+  return (
+    <div
+      id="messages-container"
+      className="overflow-y-auto flex flex-grow flex-col-reverse py-2 pr-2.5 -mr-2 gap-2"
+    >
+      {unreadMessages.map((message) => (
+        <ChatMessage key={message.id} message={message} />
+      ))}
+      {unreadMessages.length > 0 && (
+        <div className="flex flex-row items-center">
+          <hr className="flex-grow border-red-500" />
+          <p className="font-inter text-sm text-red-500 px-2">
+            {t("unread-messages", { count: unreadMessages.length })}
+          </p>
+          <hr className="flex-grow border-red-500" />
+        </div>
+      )}
+      {chat
+        .filter((message) => !unreadMessages.includes(message))
+        .map((message) => (
+          <ChatMessage key={message.id} message={message} />
+        ))}
+    </div>
+  )
+}
+
+export default ChatMessageList
