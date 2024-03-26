03cdead10e1af51eeb9a78a8cf1f740208b7df5c
maxentr <rouaultmaxent@gmail.com>
2024-03-26 16:02:32 +0100

    style(web): revamp CopyLink component

--- apps/web/components/CopyLink.tsx
+++ apps/web/components/CopyLink.tsx
@@ -1,57 +1,35 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 import { useSkyjo } from "@/contexts/SkyjoContext"
-import { cn, getGameInviteLink } from "@/lib/utils"
-import { ClassValue } from "clsx"
+import { getGameInviteLink } from "@/lib/utils"
+import { CheckIcon, ClipboardCopyIcon } from "lucide-react"
 import { useTranslations } from "next-intl"
 import { useState } from "react"
 
-type Props = { className?: ClassValue }
-
-const CopyLink = ({ className }: Props) => {
+const CopyLink = () => {
   const { game } = useSkyjo()
   const t = useTranslations("components.CopyLink")
   const [copied, setCopied] = useState(false)
-
+  const [interval, setInterval] = useState<NodeJS.Timeout>()
   const inviteLink = getGameInviteLink(window.location.href)
 
   const onCopy = () => {
     navigator.clipboard.writeText(inviteLink)
-
     setCopied(true)
 
-    setTimeout(() => {
-      setCopied(false)
-    }, 3500)
+    if (copied) clearInterval(interval)
+    setInterval(setTimeout(() => setCopied(false), 3000))
   }
 
   if (game.status === "lobby")
     return (
-      <div
-        className={cn(
-          "flex flex-col items-center justify-center gap-2 w-fit",
-          className,
-        )}
-      >
-        <p
-          className={cn(
-            "px-2.5 py-1 w-fit bg-white text-slate-800 text-sm shadow rounded-md duration-300 transition-all select-none",
-            copied
-              ? "opacity-100 translate-y-0 scale-100"
-              : "opacity-0 translate-y-4 scale-[85%]",
-          )}
-        >
-          {t("message")}
-        </p>
-        <div className="bg-white px-2 py-1 shadow rounded-md">
-          <button
-            className={cn("select-all text-sm text-slate-800")}
-            onClick={onCopy}
-            title={t("button-title")}
-          >
-            {inviteLink}
-          </button>
-        </div>
+      <div className="flex flex-row items-center gap-2">
+        <Input type="text" value={inviteLink} readOnly className="w-[300px]" />
+        <Button variant="icon" onClick={onCopy}>
+          {copied ? <CheckIcon /> : <ClipboardCopyIcon />}
+        </Button>
       </div>
     )
 }
