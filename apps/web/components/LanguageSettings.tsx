859f4c200d114d5a6aff0306ceeb89d2c1d510a9
maxentr <rouaultmaxent@gmail.com>
2024-03-26 16:02:51 +0100

    style(web): revamp LanguageSettings component

--- apps/web/components/LanguageSettings.tsx
+++ apps/web/components/LanguageSettings.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -36,10 +37,12 @@ const LanguageSettings = ({ locale }: LanguageSettingsProps) => {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger aria-label={t("button.aria-label")}>
-        <LanguagesIcon />
+      <DropdownMenuTrigger asChild>
+        <Button variant="icon" aria-label={t("button.aria-label")}>
+          <LanguagesIcon />
+        </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="mx-2">
+      <DropdownMenuContent className="mr-6">
         <DropdownMenuRadioGroup
           value={locale}
           onValueChange={handleLanguageChange}
