af7547962afff28cbcb3263c2f649ad435aeb034
maxentr <rouaultmaxent@gmail.com>
2024-04-06 01:26:57 +0200

    feat(web): add robots.txt

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -2,13 +2,13 @@ import { locales } from "@/i18n"
 import { MetadataRoute } from "next"
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const url = process.env.SITE_URL || ""
+  const baseUrl = process.env.SITE_URL ?? ""
   const pages = ["", "rules"]
 
   const sitemap = pages.map((page) => {
     return locales.map((locale) => {
       return {
-        url: `${url}/${locale}/${page}`,
+        url: `${baseUrl}/${locale}/${page}`,
         lastModified: new Date(),
         priority: 1,
       }
