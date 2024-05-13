db2424bd8d01eb84d601354b47208a73531b9eb0
maxentr <rouaultmaxent@gmail.com>
2024-05-13 11:58:15 +0200

    fix(web): robots.txt disallow urls

--- apps/web/app/robots.ts
+++ apps/web/app/robots.ts
@@ -1,9 +1,12 @@
 import { locales } from "@/i18n"
+import { DEFAULT_LOCALE } from "@/navigation"
 import { MetadataRoute } from "next"
 
 export default function robots(): MetadataRoute.Robots {
   const baseUrl = process.env.SITE_URL ?? ""
-  const disallow = locales.map((locale) => `/${locale}/game/`)
+  const disallow = locales.map((locale) =>
+    locale === DEFAULT_LOCALE ? `/game/` : `/${locale}/game/`,
+  )
 
   return {
     rules: {
