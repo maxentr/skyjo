c2befeb9e73703464b5f4a95178c9e2c09e669a7
maxentr <rouaultmaxent@gmail.com>
2024-05-13 11:26:35 +0200

    build(web): add vercel analytics and speed insights

--- apps/web/app/[locale]/providers.tsx
+++ apps/web/app/[locale]/providers.tsx
@@ -3,14 +3,20 @@
 import { Toaster } from "@/components/ui/toaster"
 import SocketContextProvider from "@/contexts/SocketContext"
 import UserContextProvider from "@/contexts/UserContext"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { PropsWithChildren } from "react"
 
 const Providers = ({ children }: PropsWithChildren) => {
   return (
-    <SocketContextProvider>
-      <UserContextProvider>{children}</UserContextProvider>
-      <Toaster />
-    </SocketContextProvider>
+    <>
+      <Analytics />
+      <SpeedInsights />
+      <SocketContextProvider>
+        <UserContextProvider>{children}</UserContextProvider>
+        <Toaster />
+      </SocketContextProvider>
+    </>
   )
 }
 
