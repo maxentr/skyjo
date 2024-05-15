cc619ca0df94213faf15fc0d5df75d1dd99beeb3
maxentr <rouaultmaxent@gmail.com>
2024-05-15 17:54:02 +0200

    style(web): underline link in accordion

--- apps/web/app/[locale]/page.tsx
+++ apps/web/app/[locale]/page.tsx
@@ -94,7 +94,7 @@ const IndexServerPage = ({ searchParams, params }: IndexServerPageProps) => {
               <AccordionTrigger>{t("faq.how-to-play.title")}</AccordionTrigger>
               <AccordionContent>
                 {t.rich("faq.how-to-play.content", {
-                  rules: (chunks) => <Link href="/rules">{chunks}</Link>,
+                  rules: (chunks) => <Link href="/rules" className="underline">{chunks}</Link>,
                 })}
               </AccordionContent>
             </AccordionItem>
