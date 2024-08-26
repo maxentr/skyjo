909677009ee9f9fcb57ac7df2804ed426b64f0e5
maxentr <rouaultmaxent@gmail.com>
2024-08-26 16:31:54 +0200

    fix(shared): update maxDateToReconnect reconnect validation

--- packages/shared/validations/reconnect.ts
+++ packages/shared/validations/reconnect.ts
@@ -3,7 +3,7 @@ import { z } from "zod"
 export const reconnect = z.object({
   gameCode: z.string(),
   playerId: z.string(),
-  maxDateToReconnect: z.string().date().optional(),
+  maxDateToReconnect: z.string().optional(),
 })
 
 export type LastGame = z.infer<typeof reconnect>
