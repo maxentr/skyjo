ea1a680782eaab241f099280190109df3d7d0053
maxentr <rouaultmaxent@gmail.com>
2024-09-11 11:11:21 +0200

    refactor(database): set DATABASE_URL as required environment variable in provider.ts

--- packages/database/src/provider.ts
+++ packages/database/src/provider.ts
@@ -5,5 +5,7 @@ import * as schema from "./schema"
 
 config({ path: ".env" })
 
-const sql = neon(process.env.DATABASE_URL!)
+if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set")
+
+const sql = neon(process.env.DATABASE_URL)
 export const db = drizzle(sql, { schema })
