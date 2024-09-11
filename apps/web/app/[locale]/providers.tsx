5d4a5787bea19bec3dbb26622995a3e4a655bbf3
maxentr <rouaultmaxent@gmail.com>
2024-09-11 10:08:01 +0200

    refactor(web): remove assertion and check if NEXT_PUBLIC_POSTHOG_KEY env var exist before using it

--- apps/web/app/[locale]/providers.tsx
+++ apps/web/app/[locale]/providers.tsx
@@ -12,8 +12,8 @@ import posthog from "posthog-js"
 import { PostHogProvider } from "posthog-js/react"
 import { PropsWithChildren } from "react"
 
-if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: "identified_only",
     persistence: "memory",
