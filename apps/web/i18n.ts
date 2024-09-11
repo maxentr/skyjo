51483e55e244a971cec3a704df3bc5c1e9240a03
maxentr <rouaultmaxent@gmail.com>
2024-09-11 11:09:30 +0200

    refactor(web): fix type in i18n.ts

--- apps/web/i18n.ts
+++ apps/web/i18n.ts
@@ -6,9 +6,9 @@ export const locales = ["en", "fr"] as const
 export type Locales = (typeof locales)[number]
 
 export default getRequestConfig(async ({ locale }) => {
-  // Validate that the incoming `locale` parameter is valid
-  // biome-ignore lint/suspicious/noExplicitAny: cannot type locale here
-  if (!locales.includes(locale as any)) notFound()
+  const localesArray = [...locales] as string[]
+
+  if (!localesArray.includes(locale)) notFound()
 
   return {
     messages: (await import(`./locales/${locale}.json`)).default,
