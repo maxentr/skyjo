fd2f74ddc8d68dd4f4dabbfb6b70a6c1e76f473f
maxentr <rouaultmaxent@gmail.com>
2024-10-13 16:07:59 +0200

    test(api): mock db env vars

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    setupFiles: "./tests/setup.ts",
     coverage: {
       reporter: ["text", "html", "json-summary", "json"],
       reportOnFailure: true,
