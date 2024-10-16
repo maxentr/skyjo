9f9c5a1a8556009874f106a045bae169ea9d8c90
maxentr <rouaultmaxent@gmail.com>
2024-10-16 23:38:14 +0200

    ci: add pipeline to trigger SonarCloud Automatic Analysis

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -1,13 +1,16 @@
-// vitest.config.ts
 import tsconfigPaths from "vite-tsconfig-paths"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    reporters: ["vitest-sonar-reporter"],
+    outputFile: {
+      "vitest-sonar-reporter": "./sonar-report.xml",
+    },
     setupFiles: "./tests/setup.ts",
     coverage: {
-      reporter: ["text", "html", "json-summary", "json"],
+      reporter: ["text", "html", "json-summary", "json", "lcov"],
       reportOnFailure: true,
       provider: "istanbul",
       reportsDirectory: "tests/coverage",
