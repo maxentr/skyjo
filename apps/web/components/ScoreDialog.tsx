8f358e317a92d2d57dd84a67a2245be82cd6f069
maxentr <rouaultmaxent@gmail.com>
2024-10-17 01:34:56 +0200

    refactor(web): remove unused code in ScoreDialog.tsx component

--- apps/web/components/ScoreDialog.tsx
+++ apps/web/components/ScoreDialog.tsx
@@ -4,16 +4,12 @@ import ScoreTable from "@/components/ScoreTable"
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog"
 import { useSkyjo } from "@/contexts/SkyjoContext"
-import { getWinner } from "@/lib/skyjo"
 import { useTranslations } from "next-intl"
 import { Dispatch, SetStateAction } from "react"
-import { GAME_STATUS, ROUND_STATUS } from "shared/constants"
-import { SkyjoPlayerToJson } from "shared/types/skyjoPlayer"
 
 type ScoreDialogProps = {
   open: boolean
@@ -24,28 +20,13 @@ const ScoreDialog = ({ open, onOpenChange }: ScoreDialogProps) => {
   const t = useTranslations("components.ScoreDialog")
   const { game } = useSkyjo()
 
-  let winner: SkyjoPlayerToJson | undefined
-  const isGameFinished =
-    game.roundStatus === ROUND_STATUS.OVER &&
-    game.status === GAME_STATUS.FINISHED
-
-  if (isGameFinished) winner = getWinner(game)
-
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle className="text-center">{t("title")}</DialogTitle>
-          {winner && (
-            <DialogDescription className="mt-2">
-              {t("description", {
-                name: winner.name,
-                score: winner.score,
-              })}
-            </DialogDescription>
-          )}
         </DialogHeader>
-        <ScoreTable players={game.players} winner={winner} />
+        <ScoreTable players={game.players} />
       </DialogContent>
     </Dialog>
   )
