7522d5bda517fbfa169846781654d00ece1bd5b2
maxentr <rouaultmaxent@gmail.com>
2024-10-20 14:49:29 +0200

    feat(web): allow user to change appearance of the website #49

---
+++ apps/web/components/AppearanceSelect.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { useTheme } from "next-themes"
+
+const AppearanceSelect = () => {
+  const { theme, setTheme } = useTheme()
+
+  return (
+    <Select value={theme} onValueChange={setTheme}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Theme" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="light">Light</SelectItem>
+        <SelectItem value="dark">Dark</SelectItem>
+        <SelectItem value="system">System</SelectItem>
+      </SelectContent>
+    </Select>
+  )
+}
+
+export { AppearanceSelect }
